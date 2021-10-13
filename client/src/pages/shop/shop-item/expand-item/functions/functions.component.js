4711407299f717cc46b8fdb051eba156505dffc2
Dino Đanić <dino.danic@gmail.com>
2021-10-13 16:57:48 +0200

    shop items with add and remove fns

--- client/src/pages/shop/shop-item/expand-item/functions/functions.component.js
+++ client/src/pages/shop/shop-item/expand-item/functions/functions.component.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../../../../../redux/shop/shop.actions";
 
 import Button from "../../../../../theme/ui-components/button/button.conponent";
-import { Wrap } from "./functions.styles";
+import { Add, Amount, Wrap } from "./functions.styles";
 
 const Functions = ({ item }) => {
+  const [amount, setAmount] = useState(1);
+  const [newItem, setNewItem] = useState({
+    ...item,
+    amount: item.amount,
+  });
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    setNewItem({
+      ...newItem,
+      amount: amount,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amount]);
+
   const handleAddToCart = () => {
-    dispatch(addItemToCart(item));
+    dispatch(addItemToCart(newItem));
+    console.log(newItem);
   };
+
+  const handleAdd = () => {
+    setAmount(amount + 1);
+  };
+
+  const handleRemove = () => {
+    if (amount === 1) return;
+    setAmount(amount - 1);
+  };
+
   return (
     <Wrap>
+      <Add onClick={handleRemove}>-</Add>
+      <Amount>{amount}</Amount>
+      <Add onClick={handleAdd}>+</Add>
       <Button onClick={handleAddToCart}>Add to Cart</Button>
     </Wrap>
   );
--- client/src/redux/shop/shop.reducer.js
+++ client/src/redux/shop/shop.reducer.js
@@ -9,32 +9,31 @@ const INITIAL_STATE = {
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case shopActionsTypes.ADD_ITEM_TO_CART:
-      const findCurrent = state.cartItems.filter(
+      console.log(action.payload);
+      const current = state.cartItems.find(
         (item) => item.id === action.payload.id
       );
-      const current = findCurrent[0];
       if (!current) {
         const newItem = {
           ...action.payload,
-          amount: 1,
+          amount: action.payload.amount || 1,
         };
         return {
           ...state,
           cartItems: [...state.cartItems, newItem],
         };
-      } else {
-        const newItem = {
-          ...current,
-          amount: current.amount + 1,
-        };
-        const deleteOld = state.cartItems.filter(
-          (item) => item.id !== action.payload.id
-        );
-        return {
-          ...state,
-          cartItems: [...deleteOld, newItem],
-        };
       }
+      const newItem = {
+        ...action.payload,
+        amount: current.amount + action.payload.amount || current.amount + 1,
+      };
+      const deleteOld = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        cartItems: [...deleteOld, newItem],
+      };
 
     case shopActionsTypes.REMOVE_ITEM_FROM_CART:
       const removeItem = state.cartItems.filter(
